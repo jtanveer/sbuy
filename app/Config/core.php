0626cc89e12463cce49d9129ccb0491c1d283890
Jamael Tanveer Nayon <imjtnayon@gmail.com>
2016-02-13 19:26:47 +0600

    first commit

--- app/Config/core.php
+++ app/Config/core.php
@@ -225,12 +225,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'TY3ROwRdQZth2LKcAOYOgSWhrXBMtpAmnDeca6p1');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '52704245446916477853373220739');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
