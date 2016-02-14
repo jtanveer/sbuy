517757b688034c9a3c94dd1102fd7d34531ef931
Jamael Tanveer Nayon <imjtnayon@gmail.com>
2016-02-15 05:22:28 +0600

    database updated

---
+++ app/Config/database.php
@@ -0,0 +1,22 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'user',
+		'password' => 'password',
+		'database' => 'test_database_name',
+	);
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'sbuy-jtanveer.rhcloud.com',
+		'port' => 3306,
+		'login' => 'adminCieXbhx',
+		'password' => '_dNF7mrygg3J',
+		'database' => 'sbuy',
+		'encoding' => 'utf8'
+	);
+}
