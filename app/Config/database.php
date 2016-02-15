2357b5ed2ec95457b2676ce65cea7e45ad98cff5
Jamael Tanveer Nayon <imjtnayon@gmail.com>
2016-02-15 15:24:41 +0600

    database updated

---
+++ app/Config/database.php
@@ -0,0 +1,34 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => '127.0.0.1',
+		'login' => 'root',
+		'port' => 3306,
+		'password' => '',
+		'database' => 'sbuy',
+		'encoding' => 'utf8'
+	);
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => '127.2.247.2',
+		'port' => 3306,
+		'login' => 'adminCieXbhx',
+		'password' => '_dNF7mrygg3J',
+		'database' => 'sbuy',
+		'encoding' => 'utf8'
+	);
+	// public $default = array(
+	// 	'datasource' => 'Database/Mysql',
+	// 	'persistent' => false,
+	// 	'host' => '127.0.0.1',
+	// 	'login' => 'root',
+	// 	'port' => 3306,
+	// 	'password' => '',
+	// 	'database' => 'sbuy',
+	// 	'encoding' => 'utf8'
+	// );
+}
